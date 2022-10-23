f2509ed0f086745dde196726d424e7b626ec1527
JAMES\james <james.lee@devncore.org>
2022-10-23 19:32:11 +0900

    Update mudblazor

---
+++ james/Jamesweb/Jamesweb/Client/Program.cs
@@ -0,0 +1,19 @@
+using Jamesweb.Client;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddHttpClient("Jamesweb.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+    .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+
+// Supply HttpClient instances that include access tokens when making requests to the server project
+builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("Jamesweb.ServerAPI"));
+
+builder.Services.AddApiAuthorization();
+builder.Services.AddMudServices();
+await builder.Build().RunAsync();
